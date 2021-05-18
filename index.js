85fb20981050ffe4e5daa8f4d316d804d83cd0c0
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-18 19:20:45 +0200

    adde Darkmode

---
+++ index.js
@@ -0,0 +1,21 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+//create arrays for day and months
+const days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+const month = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+toggle.addEventListener('click', (e) => {
+	const html = document.querySelector('html');
+	if (html.classList.contains('dark')) {
+		html.classList.remove('dark');
+		e.target.innerHTML = 'Dark Mode';
+	} else {
+		html.classList.add('dark');
+		e.target.innerHTML = 'Light Mode';
+	}
+});
